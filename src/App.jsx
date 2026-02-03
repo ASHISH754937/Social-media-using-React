e87ad6710a717b339b9ae232f304434f9ba75e35
ASHISH754937 <22bcs11958@cuchd.in>
2026-02-03 23:51:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import"bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
+import { useState } from 'react';
+import PostListPrivider from './store/Post-list-store';
+function App() {
+  const [selectedTab,setSelectedTab]=useState("Home");
+
+  return (
+    <PostListPrivider>
+      <div className="app-container">
+        <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab==='Home'?(<PostList></PostList>):(<CreatePost></CreatePost>)}
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListPrivider>
+  );
+}
+
+export default App
